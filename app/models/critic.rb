5e4cb92703b9f024b31437aed8ea8a444bca5aa8
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-19 18:28:19 -0500

    Added validations to the Critic table

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 class Critic < ApplicationRecord
   belongs_to :game
+  validates :username,
+            length: { in: 2...12 },
+            format: { with: /^[a-zA-Z\s0-9]+$/,
+                      message: 'only allows alphanumeric values' }
+  validates :title, :body, presence: { message: 'Please write something!' }
 end
