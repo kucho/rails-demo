97bce9a1bf339e2d3d2401f9c4ca1b59fed9942a
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-19 17:42:16 -0500

    Added Company validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Company < ApplicationRecord
+  validates :name,
+            uniqueness: true,
+            presence: true,
+            length: { maximum: 40 }
+
+  validates :country, presence: true
 end
