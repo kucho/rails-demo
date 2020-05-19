2d5b5cda081d6f255a9fff0c54d7a04b12e2a1e6
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-19 17:51:22 -0500

    Added validations to the Game table

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Game < ApplicationRecord
+  validate :name, :genre, :price, :release_date, :company, precense: true
+  validate :genre,
+           inclusion: { in: %w(Simulator Adventure
+                               Strategy Role-playing (RPG)
+                               Shooter Fighting Sport),
+                        message: '%<value> is not a valid genre' }
+  validate :price, numericality: { greater_than: 0 }
 end
