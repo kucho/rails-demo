f16e6470c7d644c4ef2796750aa9f0a0e59093df
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-20 16:12:30 -0500

    Fix typos in validates

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -4,7 +4,7 @@ class Critic < ApplicationRecord
   belongs_to :game
   validates :username,
             length: { in: 2...12 },
-            format: { with: /^[a-zA-Z\s0-9]+$/,
+            format: { with: /A^[a-zA-Z\s0-9]$Z/,
                       message: 'only allows alphanumeric values' }
   validates :title, :body, presence: { message: 'Please write something!' }
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,11 +3,11 @@
 class Game < ApplicationRecord
   belongs_to :company
   has_many :critics
-  validate :name, :genre, :price, :release_date, :company, precense: true
-  validate :genre,
+  validates :name, :genre, :price, :release_date, :company, presence: true
+  validates :genre,
            inclusion: { in: %w(Simulator Adventure
                                Strategy Role-playing (RPG)
                                Shooter Fighting Sport),
                         message: '%<value> is not a valid genre' }
-  validate :price, numericality: { greater_than: 0 }
+  validates :price, numericality: { greater_than: 0 }
 end
