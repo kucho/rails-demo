4db0e03dbe431f2e58408134e485315d83eebc08
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-20 19:18:10 -0500

    Added routes to create

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,27 @@
 class CompaniesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
-    @companies = Company.all # Get all the games and saved them on @games
-    render json: @companies # Render all the games on JSON format
+    @companies = Company.all # Get all the companies and saved them on @games
+    render json: @companies # Render all the companies on JSON format
+  end
+
+  def show
+    @company = Company.find(params[:id]) # Get an specific company using the id of the URL
+    render json: @company # render the specific company using json format
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      render json: @company
+    else
+      render json: @company.errors
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description, :country, :start_date)
   end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,29 @@
 class GamesController < ApplicationController
+  #before_action :set_game, only: %i[show edit update destroy]
+  skip_before_action :verify_authenticity_token
   def index
     @games = Game.all   # Get all the games and saved them on @games
     render json: @games # Render all the games on JSON format
+  end
+
+  def show
+    @game = Game.find(params[:id]) # Get an specific game using the id of the URL
+    render json: @game # render the specific game using json format
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      render json: @game
+    else
+      render json: @game.errors
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :genre, :price,
+                                 :release_date, :company_id)
   end
 end
