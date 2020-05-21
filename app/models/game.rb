ba44c7fb8a834d2b43eb5e5e2e71ae991778a8db
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-20 19:47:52 -0500

    Fixed bug in valid genres

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,9 +5,9 @@ class Game < ApplicationRecord
   has_many :critics
   validates :name, :genre, :price, :release_date, :company, presence: true
   validates :genre,
-            inclusion: { in: %w(Simulator Adventure
-                                Strategy Role-playing (RPG)
-                                Shooter Fighting Sport),
+            inclusion: { in: ['Simulator', 'Adventure',
+                              'Strategy', 'Role-playing (RPG)',
+                              'Shooter', 'Fighting', 'Sport'],
                          message: '%<value> is not a valid genre' }
   validates :price, numericality: { greater_than: 0 }
 end
